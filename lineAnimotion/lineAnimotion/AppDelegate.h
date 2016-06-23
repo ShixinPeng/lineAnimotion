2cdca9cd63ce40be73b67c167963cc5536d7902a
pengshixin <shixinpeng0429@163.com>
2016-06-23 10:42:34 +0800

    creat subject

---
+++ lineAnimotion/lineAnimotion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lineAnimotion
+//
+//  Created by pengshixin on 16/6/23.
+//  Copyright © 2016年 pengshixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lineAnimotion/lineAnimotion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lineAnimotion
+//
+//  Created by pengshixin on 16/6/23.
+//  Copyright © 2016年 pengshixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
